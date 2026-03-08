8babf8a2d4872cb19119cdd1ed901b6ff88eb750
Manoncey <venturamanon@gmail.com>
2026-03-08 14:41:29 +0100

    chore: add dinner icon

--- app/utils.ts
+++ app/utils.ts
@@ -2,16 +2,18 @@ import type { AmenityIcon } from "./types";
 import WifiIcon from "@mui/icons-material/Wifi";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
 import RestaurantIcon from "@mui/icons-material/Restaurant";
+import FreeBreakfastIcon from '@mui/icons-material/FreeBreakfast';
 import LocalTaxiIcon from "@mui/icons-material/LocalTaxi";
 import HotTubIcon from "@mui/icons-material/HotTub";
 import KitchenIcon from "@mui/icons-material/Kitchen";
 import TvIcon from "@mui/icons-material/Tv";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-// This is just a plain object now. Much simpler.
+
 const ICON_MAP: Record<AmenityIcon, React.ElementType> = {
   Wifi: WifiIcon,
   AcUnit: AcUnitIcon,
-  Breakfast: RestaurantIcon,
+  Breakfast: FreeBreakfastIcon,
+  Dinner: RestaurantIcon,
   Taxi: LocalTaxiIcon,
   HotWater: HotTubIcon,
   Fridge: KitchenIcon,
